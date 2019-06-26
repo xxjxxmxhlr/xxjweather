11b6110024997438920581c0b828e203274ebe10
xxjxxmxhlr <xxjxxmxhlr@163.com>
2019-06-26 11:38:11 +0800

    first commit

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,14 @@
+function isEmptyval(val){
+    var result = false;
+    if(val==null||val==undefined||val==""||val.length==0){
+        result = true;
+    }
+    return result;
+}
+
+//获取url中的参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r != null) return unescape(r[2]); return null; //返回参数值
+}
